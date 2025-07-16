4919a6aa4ce7dd4883f4d722978435f8749f1585
SAHIL <154607992+ItzSahil101@users.noreply.github.com>
2025-07-16 22:55:07 +0545

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -106,13 +106,13 @@ router.post("/login", async (req, res) => {
       expiresIn: "7d",
     });
 
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-      })
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // will be true in production
+  sameSite: "none", // required for cross-site cookies on HTTPS
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
       .json({ msg: "Login successful" });
   } catch (err) {
     res.status(500).json({ msg: "Login failed", error: err.message });
