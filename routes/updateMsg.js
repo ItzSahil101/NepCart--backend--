445a48b62f85c78e4607df4a433782e3893d6114
ItzSahil101 <sahilminecraft502@gmail.com>
2025-10-15 16:04:23 +0545

    abc

--- routes/updateMsg.js
+++ routes/updateMsg.js
@@ -4,10 +4,9 @@ const router = express.Router();
 const UpdateMsg = require("../models/Update");
 
 // GET request instead of POST
-router.get("/fetch", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const updates = await UpdateMsg.find();
-    res.json(updates);
+    res.json("Fetching updates...");
   } catch (error) {
     console.error("Fetch updates error:", error);
     res.status(500).json({ error: error.message });
